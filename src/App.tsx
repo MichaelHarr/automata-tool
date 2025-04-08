91b0b30c6ff19f36fe5324f106983626291bd5d3
MichaelHarr <michaelharrison2717@gmail.com>
2025-04-08 19:26:24 +0100

    Add check for strings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,74 @@
 import './index.css'
 import Flow from './Flow';
-import React, { useEffect, useState } from 'react';
-import { ReactFlowProvider } from '@xyflow/react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { ReactFlowProvider, 
+        useEdgesState, 
+        useNodesState,
+        Node as FlowNode,
+        Edge as FlowEdge,
+        MarkerType,
+        addEdge,
+        applyNodeChanges,
+        applyEdgeChanges, } from '@xyflow/react';
+
+interface CustomNodeData {
+    label: string;
+    initialState: boolean;
+    finalState: boolean;
+    [key: string]: string | boolean; 
+}
+
+interface CustomEdgeData {
+    label: string;
+    [key: string]: unknown;
+}
+
+const initialNodes: FlowNode<CustomNodeData>[] = [
+    {
+      id: '1',
+      type: 'circleNode',
+      data: { label: 'Node 1', initialState: false, finalState: false },
+      position: { x: 250, y: 25 },
+    },
+   
+    {
+      id: '2',
+      type: 'circleNode',
+      data: { label: 'Node 2', initialState: false, finalState: false },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'circleNode',
+      data: { label: 'Node 3', initialState: false, finalState: false },
+      position: { x: 250, y: 250 },
+    },
+];
+   
+const initialEdges: FlowEdge<CustomEdgeData>[] = [
+    { 
+        id: 'e1-2', 
+        source: '1', 
+        target: '2', 
+        animated: true, 
+        markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20},     
+        label: '1'
+    },
+    { 
+        id: 'e2-3',     
+        source: '2', 
+        target: '3', 
+        animated: true, 
+        markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20},
+        label: '0'
+    }
+];
+
+interface MenuType {
+    id: string;
+    top: number;
+    left: number;
+}
 
 function App() {
 
@@ -12,15 +79,131 @@ function App() {
 
   const [inputString, setInputString] = useState('');
   const [isValid, setIsValid] = useState<boolean>(false);
-  const [testStringFunction, setTestStringFunction] = useState<((inputString: string) => boolean) | null>(null);
+  const [testStringFunction, setTestStringFunction] = useState<(input: string) => boolean>(() => () => false);
 
-  const onDragStart = (event: React.DragEvent) => {
-    event.dataTransfer.effectAllowed = 'move';
-  }
 
-  const checkInputString = (inputString: string) => {
-    console.log("Input String: ", inputString); 
-  }
+  const [nodes, setNodes, onNodesChange] = useNodesState<FlowNode<CustomNodeData>>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<FlowEdge<CustomEdgeData>>(initialEdges);
+
+  const nodesRef = useRef(nodes);
+  const edgesRef = useRef(edges);
+
+  const nodeIdCounter = useRef(nodes.length);
+
+  const [menu, setMenu] = useState<MenuType | null>(null);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const onConnect = useCallback(
+      (connection: any) => {
+          const newEdge = {
+              ...connection,
+              markerEnd: {type: MarkerType.ArrowClosed, width: 20, height: 20},
+              animated: true,
+              label: '0',
+          }
+          setEdges((eds) => addEdge(newEdge, eds))
+      },
+      [setEdges]
+  );
+  
+  const onDragOver = useCallback((event: any) => {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onNodeContextMenu = useCallback(
+      (event: any, node: any) => {
+          event.preventDefault();
+
+          const pane = ref.current?.getBoundingClientRect();
+
+          console.log("Pane", pane);
+          if (!pane) return;
+
+          const x = event.clientX - pane.left;
+          const y = event.clientY - pane.top;
+
+          setMenu({
+              id: node.id,
+              top: y,
+              left: x,
+          })
+      }, [setMenu],
+  );
+
+  const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+
+  const onDragStart = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.dataTransfer.setData('application/reactflow', 'circleNode');
+  }, []);
+
+  const checkInputString = useCallback((input: string) => {
+    console.log("Input String:", input);
+    console.log("Nodes:", nodes);
+    let currentNode = nodes.find(node => node.data.initialState);
+
+    let transitionFound: boolean = true;
+
+    for (let i = 0; i < input.length; i++) {
+
+        const transition = edges.find(edge => edge.source === currentNode?.id && edge.label === input[i]);
+
+        if (transition) {
+          currentNode = nodes.find(node => node.id === transition.target);
+          transitionFound = true;
+        } else {
+          console.log("No transition found for input:", input[i]);
+        }
+    }
+
+    const isAccepted = transitionFound && currentNode?.data.finalState;
+    setIsValid(isAccepted ?? false);
+    console.log(isAccepted ? "Accepted" : "Not Accepted");
+
+
+  }, [nodes, edges]);
+
+  // Update node label when nodeName changes
+  useEffect(() => {
+      setNodes((nds) =>
+        nds.map((node) => {
+          if (node.id === selectedNodeId) {
+            return {
+              ...node,
+              data: {
+                ...node.data,
+                label: nodeName,
+              },
+            };
+          }
+    
+          return node;
+        }),
+      );
+    }, [setNodes, nodeName, selectedNodeId]);
+
+  // Update edge label when edgeName changes
+  useEffect(() => {
+      setEdges((eds) =>
+        eds.map((edge) => {
+          if (edge.id === selectedEdgeId) {
+            return {
+              ...edge,
+              label: edgeName
+            };
+          }
+          return edge;
+        }),
+      );
+    }, [setEdges, edgeName, selectedEdgeId]);
+
+  useEffect(() => {
+    nodesRef.current = nodes;
+  }, [nodes]);
+  
+  useEffect(() => {
+    edgesRef.current = edges;
+  }, [edges]);
 
   return (
     <>
@@ -37,7 +220,7 @@ function App() {
             <input
               type="text"
               placeholder="Input String"
-              className="border border-gray-300 p-2 rounded"
+              className={`border border-gray-300 p-2 rounded ${isValid ? 'bg-green-800' : 'bg-transparent'}`}
               value={inputString}
               onChange={(event) => setInputString(event.target.value)}
             />
@@ -75,17 +258,27 @@ function App() {
       </div>
       <ReactFlowProvider>
         <Flow 
-          nodeName={nodeName}
-          setNodeName={setNodeName}
-          selectedNodeId={selectedNodeId}
-          setSelectedNodeId={setSelectedNodeId}
-          edgeName={edgeName}
-          setEdgeName={setEdgeName}
-          selectedEdgeId={selectedEdgeId}
-          setSelectedEdgeId={setSelectedEdgeId}
-          inputString={inputString}
-          setTestStringFunction={setTestStringFunction}
-        />
+            ref={ref}
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            nodeName={nodeName}
+            setNodeName={setNodeName}
+            selectedNodeId={selectedNodeId}
+            setSelectedNodeId={setSelectedNodeId}
+            edgeName={edgeName}
+            setEdgeName={setEdgeName}
+            selectedEdgeId={selectedEdgeId}
+            setSelectedEdgeId={setSelectedEdgeId}
+            inputString={inputString}
+            onConnect={onConnect}
+            onDragOver={onDragOver}
+            setNodes={setNodes}
+            onNodeContextMenu={onNodeContextMenu}
+            onPaneClick={onPaneClick}
+            menu={menu}        
+          />
       </ReactFlowProvider>
     </div>
   </>
